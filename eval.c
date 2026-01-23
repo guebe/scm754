ef98a3384542c0003505b8ae32a23e94d0542f8a
guebe <57858743+guebe@users.noreply.github.com>
2026-01-23 11:35:44 +0100

    moved procedures step 2

--- eval.c
+++ eval.c
@@ -3,20 +3,9 @@
 #undef DEBUG
 
 #include "scm754.h"
-#include <math.h>
 #ifdef DEBUG
 #include <stdio.h>
 #endif
-
-static size_t list_length(scm_obj_t list)
-{
-	size_t i = 0;
-	while (scm_is_pair(list)) {
-		list = scm_cdr(list);
-		i++;
-	}
-	return i;
-}
 
 static scm_obj_t eval_list(scm_obj_t list, scm_obj_t environment_specifier, scm_obj_t argv[])
 {
@@ -105,7 +94,7 @@ printf("alloc2: 1\n");
 		/* application */
 		scm_obj_t proc = scm_eval(op, environment_specifier);
 		if (scm_is_error_object(proc)) return proc;
-		size_t argc = list_length(args);
+		size_t argc = scm_length(args);
 		scm_obj_t argv[argc > 0 ? argc : 1];
 		scm_obj_t ret = eval_list(args, environment_specifier, argv);
 		if (scm_is_error_object(ret)) return ret;
@@ -118,98 +107,6 @@ printf("alloc2: 1\n");
 		/* self-evaluating */
 		return expr_or_def;
 	}
-}
-
-extern scm_obj_t scm_add(scm_obj_t argv[], size_t argc)
-{
-	double x = 0.0;
-	for (size_t i = 0; i < argc; i++) {
-		scm_obj_t a = argv[i];
-		if (!scm_is_number(a)) return scm_error("+: needs a number");
-		x += scm_number_value(a);
-	}
-	return scm_number(x);
-}
-
-extern scm_obj_t scm_sub(scm_obj_t argv[], size_t argc)
-{
-	if (argc == 0) return scm_error("-: needs an argument");
-	scm_obj_t a = argv[0];
-	if (!scm_is_number(a)) return scm_error("-: needs a number");
-	double x = scm_number_value(a);
-	if (argc == 1) return scm_number(-x);
-
-	for (size_t i = 1; i < argc; i++) {
-		a = argv[i];
-		if (!scm_is_number(a)) return scm_error("-: needs a number");
-		x -= scm_number_value(a);
-	}
-	return scm_number(x);
-}
-
-extern scm_obj_t scm_mul(scm_obj_t argv[], size_t argc)
-{
-	double x = 1.0;
-	for (size_t i = 0; i < argc; i++) {
-		scm_obj_t a = argv[i];
-		if (!scm_is_number(a)) return scm_error("*: needs a number");
-		x *= scm_number_value(a);
-	}
-	return scm_number(x);
-}
-
-extern scm_obj_t scm_div(scm_obj_t argv[], size_t argc)
-{
-	if (argc == 0) return scm_error("/: needs an argument");
-	scm_obj_t a = argv[0];
-	if (!scm_is_number(a)) return scm_error("/: needs a number");
-	double x = scm_number_value(a);
-	if (argc == 1) return scm_number(1.0/x);
-
-	for (size_t i = 1; i < argc; i++) {
-		a = argv[i];
-		if (!scm_is_number(a)) return scm_error("/: needs a number");
-		double y = scm_number_value(a);
-		if (y == 0.0) return scm_error("/: division by zero");
-		x /= y;
-	}
-	return scm_number(x);
-}
-
-extern scm_obj_t scm_numeric_equal(scm_obj_t argv[], size_t argc)
-{
-	if (argc == 0) return scm_error("=: needs an argument");
-	scm_obj_t a = argv[0];
-	if (!scm_is_number(a)) return scm_error("=: needs a number");
-	double x = scm_number_value(a);
-	if (argc == 1) return scm_true();
-
-	for (size_t i = 1; i < argc; i++) {
-		a = argv[i];
-		if (!scm_is_number(a)) return scm_error("=: needs a number");
-		if (x != scm_number_value(a)) return scm_false();
-	}
-	return scm_true();
-}
-
-extern scm_obj_t scm_quotient(scm_obj_t a, scm_obj_t b)
-{
-	double x, y;
-	if (!scm_is_number(a) || !scm_is_number(b)) return scm_error("quotient: needs two numbers");
-	x = scm_number_value(a);
-	y = scm_number_value(b);
-	if (y == 0.0) return scm_error("quotient: division by zero");
-	return scm_number(trunc(x / y));
-}
-
-extern scm_obj_t scm_modulo(scm_obj_t a, scm_obj_t b)
-{
-	double x, y;
-	if (!scm_is_number(a) || !scm_is_number(b)) return scm_error("modulo: needs two numbers");
-	x = scm_number_value(a);
-	y = scm_number_value(b);
-	if (y == 0.0) return scm_error("modulo: division by zero");
-	return scm_number(x - y * floor(x / y));
 }
 
 extern scm_obj_t scm_apply(scm_obj_t proc, scm_obj_t argv[], size_t argc)
--- scm754.h
+++ scm754.h
@@ -154,5 +154,6 @@ extern scm_obj_t scm_numeric_equal(scm_obj_t argv[], size_t argc);
 extern scm_obj_t scm_quotient(scm_obj_t a, scm_obj_t b);
 extern scm_obj_t scm_modulo(scm_obj_t a, scm_obj_t b);
 static inline scm_obj_t scm_is_eq(scm_obj_t obj1, scm_obj_t obj2) { return scm_boolean(obj1 == obj2); }
+extern size_t scm_length(scm_obj_t list);
 
 #endif
