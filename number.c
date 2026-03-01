b0c52e5e0f92b2983d6e9c0930f159e8cfebd400
guebe <57858743+guebe@users.noreply.github.com>
2026-03-01 04:53:31 +0100

    moved functions

--- number.c
+++ number.c
@@ -21,3 +21,117 @@ extern scm_obj_t scm_string_to_number(const char *string, int radix)
 
 	return scm_number(value);
 }
+
+extern scm_obj_t scm_number_to_string(scm_obj_t number)
+{
+	if (!scm_is_number(number)) return scm_error("number->string: needs a number");
+	char buffer[64];
+	int ret = snprintf(buffer, sizeof(buffer), "%.16g", scm_number_value(number));
+	if (ret < 0 || (size_t)ret >= sizeof(buffer)) return scm_error("number->string: number too big");
+	return scm_string(buffer, (size_t)ret);
+}
+
+extern scm_obj_t scm_add(scm_obj_t args)
+{
+	double x = 0.0;
+	while (scm_is_pair(args)) {
+		scm_obj_t a = scm_car(args);
+		if (!scm_is_number(a)) return scm_error("+: needs a number");
+		x += scm_number_value(a);
+		args = scm_cdr(args);
+	}
+	return scm_number(x);
+}
+
+extern scm_obj_t scm_mul(scm_obj_t args)
+{
+	double x = 1.0;
+	while (scm_is_pair(args)) {
+		scm_obj_t a = scm_car(args);
+		if (!scm_is_number(a)) return scm_error("*: needs a number");
+		x *= scm_number_value(a);
+		args = scm_cdr(args);
+	}
+	return scm_number(x);
+}
+
+extern scm_obj_t scm_sub(scm_obj_t args)
+{
+	scm_obj_t a = scm_car(args);
+	if (!scm_is_number(a)) return scm_error("-: needs a number");
+	double x = scm_number_value(a);
+	args = scm_cdr(args);
+	if (scm_is_null(args)) return scm_number(-x);
+
+	while (scm_is_pair(args)) {
+		a = scm_car(args);
+		if (!scm_is_number(a)) return scm_error("-: needs a number");
+		x -= scm_number_value(a);
+		args = scm_cdr(args);
+	}
+	return scm_number(x);
+}
+
+extern scm_obj_t scm_div(scm_obj_t args)
+{
+	scm_obj_t a = scm_car(args);
+	if (!scm_is_number(a)) return scm_error("/: needs a number");
+	double x = scm_number_value(a);
+	args = scm_cdr(args);
+	if (scm_is_null(args)) return scm_number(1.0/x);
+
+	while (scm_is_pair(args)) {
+		a = scm_car(args);
+		if (!scm_is_number(a)) return scm_error("/: needs a number");
+		double y = scm_number_value(a);
+		if (y == 0.0) return scm_error("/: division by zero");
+		x /= y;
+		args = scm_cdr(args);
+	}
+	return scm_number(x);
+}
+
+extern scm_obj_t scm_max(scm_obj_t args)
+{
+	scm_obj_t a = scm_car(args);
+	if (!scm_is_number(a)) return scm_error("max: needs a number");
+	double x = scm_number_value(a);
+	args = scm_cdr(args);
+
+	while (scm_is_pair(args)) {
+		a = scm_car(args);
+		if (!scm_is_number(a)) return scm_error("max: needs a number");
+		double y = scm_number_value(a);
+		if (y > x) x = y;
+		args = scm_cdr(args);
+	}
+	return scm_number(x);
+}
+
+extern scm_obj_t scm_quotient(scm_obj_t a, scm_obj_t b)
+{
+	double x, y;
+	if (!scm_is_number(a) || !scm_is_number(b)) return scm_error("quotient: needs two numbers");
+	x = scm_number_value(a);
+	y = scm_number_value(b);
+	if (y == 0.0) return scm_error("quotient: division by zero");
+	return scm_number(trunc(x / y));
+}
+
+extern scm_obj_t scm_modulo(scm_obj_t a, scm_obj_t b)
+{
+	double x, y;
+	if (!scm_is_number(a) || !scm_is_number(b)) return scm_error("modulo: needs two numbers");
+	x = scm_number_value(a);
+	y = scm_number_value(b);
+	if (y == 0.0) return scm_error("modulo: division by zero");
+	return scm_number(x - y * floor(x / y));
+}
+
+extern scm_obj_t scm_is_zero(scm_obj_t z)
+{
+	if (!scm_is_number(z)) return scm_error("zero?: needs a number");
+	double x = scm_number_value(z);
+	return scm_boolean(x == 0.0);
+}
+
--- string.c
+++ string.c
@@ -28,3 +28,71 @@ extern scm_obj_t scm_string(const char *string, size_t k)
 	strings[i] = s;
 	return SCM_STRING | i;
 }
+
+extern scm_obj_t scm_substring(scm_obj_t args)
+{
+	scm_obj_t a = scm_car(args);
+	if (!scm_is_string(a)) return scm_error("string-copy: needs a string");
+	const char *x = scm_string_value(a);
+
+	size_t start = 0;
+	size_t max = scm_string_length(a);
+	size_t end;
+	bool end_avail = false;
+
+	args = scm_cdr(args);
+	if (!scm_is_null(args)) {
+		scm_obj_t b = scm_car(args);
+		if (!scm_is_number(b)) return scm_error("substring: needs a number");
+		start = scm_number_to_size(b);
+		if (start == SIZE_MAX) return scm_error("substring: can't convert to size_t");
+
+		args = scm_cdr(args);
+		if (!scm_is_null(args)) {
+			scm_obj_t c = scm_car(args);
+			if (!scm_is_number(c)) return scm_error("substring: needs a number");
+			end = scm_number_to_size(c);
+			if (end == SIZE_MAX) return scm_error("substring: can't convert to size_t");
+
+			if (!scm_is_null(scm_cdr(args))) return scm_error("substring: too many arguments");
+			end_avail = true;
+		}
+	}
+
+	if (!end_avail) end = max;
+
+	if (start > end || end > max) return scm_error("string-copy: invalid arguements");
+
+	return scm_string(x + start, end - start);
+}
+
+extern scm_obj_t scm_string_ref(scm_obj_t string, scm_obj_t k)
+{
+	if (!scm_is_string(string) || !scm_is_number(k)) return scm_error("string-ref: type err");
+
+	const char *s = scm_string_value(string);
+	size_t len = scm_string_length(string);
+	size_t i = scm_number_to_size(k);
+	if (i == SIZE_MAX) return scm_error("string-ref: can't convert to size_t");
+
+	if (i >= len)
+		return scm_error("string-ref: index %lu out of bounds (string length %lu)", i, len);
+
+	return scm_char(s[i]);
+}
+
+extern scm_obj_t scm_string_set(scm_obj_t string, scm_obj_t k, scm_obj_t c)
+{
+	if (!scm_is_string(string) || !scm_is_number(k) || !scm_is_char(c)) return scm_error("string-set!: type err");
+
+	char *s = scm_string_value(string);
+	size_t len = scm_string_length(string);
+	size_t i = scm_number_to_size(k);
+	if (i == SIZE_MAX) return scm_error("string-set!: can't convert to size_t");
+
+	if (i >= len)
+		return scm_error("string-set!: index %lu out of bounds (string length %lu)", i, len);
+
+	s[i] = (char)scm_char_value(c);
+	return scm_unspecified();
+}
